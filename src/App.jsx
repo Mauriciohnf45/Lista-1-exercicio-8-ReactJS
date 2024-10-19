89b55dd7497b4d4f8fceba49418088b31ce9d8ef
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 09:02:26 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StatusUsuario from './components/StatusUsuario';
+
+function App() {
+  return (
+    <div className="App">
+      <StatusUsuario online={true} />
+    </div>
+  );
+}
+
+export default App;
